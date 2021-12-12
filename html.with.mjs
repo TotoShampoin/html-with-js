2b2c5b5d3b6405b65e6ed6e3080caa1c90463513
TotoShampoin <thomasgarnier2010@hotmail.fr>
2021-12-13 00:50:36 +0100

    the main module code

---
+++ html.with.mjs
@@ -0,0 +1,57 @@
+// Copyright 2021 TotoShampoin
+// 
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, 
+// provided that the above copyright notice and this permission notice appear in all copies.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. 
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, 
+// WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+/**
+ * A function that calls doument.createElement and receives its attributes, datas, etc... in form of an object argument
+ * @param {string} tagName the name of the tag of the HTML element
+ * @param {string|HTMLElement|HTMLElement[]} content the content of the HTML element, be it text or another HTML element, or even an array of HTML elements
+ * @param {object} options HTML element options that you would normally put line by line in vanilla code
+ * @returns {HTMLElement} an HTML element
+ */
+export default (tagName = "div", content = [], options = {}) => {
+    const el = document.createElement(tagName);
+    for(let o in options) {
+        switch(o) {
+            default:
+                el[o] = options[o];
+                break;
+            case "classList":
+                if(typeof options.classList === 'string') {
+                    el.classList.value = options.classList;
+                }
+                if(options.classList instanceof Array) {
+                    el.classList.add(options.classList);
+                }
+                break;
+            case "dataset":
+                for(let d in options.dataset) {
+                    el.dataset[d] = options.dataset[d];
+                }
+                break;
+            case "style":
+                for(let s in options.style) {
+                    el.style[s] = options.style[s];
+                    el.style.setProperty(s, options.style[s]);
+                }
+                break;
+        }
+    }
+    if(typeof content === 'string') {
+        el.innerText = content;
+    } else if(content instanceof HTMLElement) {
+        el.appendChild(content);
+    } else if(content instanceof Array) {
+        content.forEach(c => {
+            if(c instanceof HTMLElement) {
+                el.appendChild(c);
+            } else throw new Error("invalid content");
+        })
+    } else throw new Error("invalid content");
+    return el;
+}
